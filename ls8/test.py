872739fdad54ef57da16fbd6c0bd8109f5df7d1d
ngaivoronski <ngaivoronski@gmail.com>
2020-04-22 22:16:23 -0400

    testing argcount

--- ls8/test.py
+++ ls8/test.py
@@ -1,18 +1,25 @@
 import sys
+import inspect
 
-with open("./examples/print8.ls8") as f:
-    program = f.readlines()
+# with open("./examples/print8.ls8") as f:
+#     program = f.readlines()
 
-newarr = [0] * len(program)
+# newarr = [0] * len(program)
 
-for i in range(len(program)):
-    if "#" in program[i]:
-        newarr[i] = program[i].split("#")[0]
-    elif " #" in program[i]:
-        newarr[i] = program[i].split(" #")[0]
-    elif "\n" in program[i]:
-        newarr[i] = program[i].split("\n")[0]
-    else:
-        newarr[i] = program[i]
+# for i in range(len(program)):
+#     if "#" in program[i]:
+#         newarr[i] = program[i].split("#")[0]
+#     elif " #" in program[i]:
+#         newarr[i] = program[i].split(" #")[0]
+#     elif "\n" in program[i]:
+#         newarr[i] = program[i].split("\n")[0]
+#     else:
+#         newarr[i] = program[i]
 
-print(newarr)
+def testfunc(x,y,z):
+    return x+y+z
+
+def testfunc2(x, y):
+    return x+y
+
+print(testfunc2.__code__.co_argcount)
